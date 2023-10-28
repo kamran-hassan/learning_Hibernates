9856f194d0e7fbb7452f6497a28dbeb2d8691f65
kamran-hassan <hassan.kamran107@gmail.com>
2023-10-28 10:24:35 +0530

    testing

---
+++ myHibernateProject/src/main/java/com/testHibernate/myHibernateProject/MyHibernateProjectApplication.java
@@ -0,0 +1,15 @@
+package com.testHibernate.myHibernateProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHibernateProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MyHibernateProjectApplication.class, args);
+		System.out.println("Start Learning Hibernate");
+	}
+
+}
---
+++ myHibernateProject/src/test/java/com/testHibernate/myHibernateProject/MyHibernateProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.testHibernate.myHibernateProject;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.Ignore;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHibernateProjectApplicationTests {
+
+	@Ignore
+	@Test
+	void contextLoads() {
+	}
+
+}
